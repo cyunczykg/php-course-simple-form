6ffde0cb6274168da92af9ee074acdab1de8083c
Grzegorz Cyuńczyk <cyunczykg@gmail.com>
2020-04-16 12:19:02 +0200

    completed form with simple audio player addon

--- order.php
+++ order.php
@@ -3,15 +3,35 @@
 <head>
 	<meta charset="utf-8" />
 	<title>Podsumowanie zamówienia</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
 	
 	
 	<?php
-		$paczkow = $_POST['ilepoczkow'];
+		$paczkow = $_POST['ilepaczkow'];
 		$eklerkow = $_POST['ileeklerkow'];	
-	echo "<h2>Pączków: $poczkow Eklerków: $eklerkow<h2>"
-	?>
+		$lacznie = $paczkow*0.99+$eklerkow*1.99;
+echo<<<END
 
+	<h2>Podsumowanie zamowienia<h2>
+	<table>
+		<tr>
+			<td>Pączków (0,99PLN/szt.)</td>
+			<td align="right">$paczkow</td>
+		</tr>
+		<tr>
+			<td>Eklerków (1,99PLN/szt.)</td>
+			<td align="right">$eklerkow</td>
+		</tr>
+		<tr>
+			<td>Wartość zamówienia:</td>
+			<td>$lacznie</td>
+		</tr>		
+	</table>
+END;
+	?>
+	</br>
+	<a href="index.html" alt="Modyfikuj zamówienie">Przelicz ponownie</a>
 </body>
 </html>
